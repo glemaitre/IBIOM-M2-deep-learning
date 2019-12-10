853a01e97ba6f7abf2fb001706945ee616bf8ce1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-12-11 00:03:26 +0100

    update

--- solutions/01_03.py
+++ solutions/01_03.py
@@ -1,3 +1,5 @@
-coefs = np.linalg.solve(decision_points,
-                        np.ones(shape=(decision_points.shape[0], 1)))
+coefs = np.linalg.solve(
+    decision_points,
+    np.ones(shape=(decision_points.shape[0], 1))
+)
 coefs
